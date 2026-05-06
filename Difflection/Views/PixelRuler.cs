d3bcca68a1b1a1928435587d0d21a9aeae8a85b2
Toby <github.periplexed802@passmail.net>
2026-05-06 16:14:57 +1000

    Use FontFamily.Default  in PixelRuler

--- Difflection/Views/PixelRuler.cs
+++ Difflection/Views/PixelRuler.cs
@@ -121,7 +121,7 @@ public sealed class PixelRuler : Control
             return;
         }
 
-        var typeface = new Typeface("Inter");
+        var typeface = new Typeface(FontFamily.Default);
 
         context.FillRectangle(Background ?? DefaultBackgroundBrush, bounds);
 
